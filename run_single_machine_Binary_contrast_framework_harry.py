3b980c079e58a4cc7324f48cfa5c919a157b2d13
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-11-19 14:14:07 +0800

    commit

--- run_single_machine_Binary_contrast_framework_harry.py
+++ run_single_machine_Binary_contrast_framework_harry.py
@@ -22,6 +22,8 @@ import objective as obj_lib
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
         tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
--- run_single_machine_baseline.py
+++ run_single_machine_baseline.py
@@ -24,6 +24,8 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 if gpus:
 
     try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
         tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
