e27f211c65a5bc7ce74318ae81b27d57e3e95f1b
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-11-19 14:15:07 +0800

    commit

--- run_single_machine_baseline.py
+++ run_single_machine_baseline.py
@@ -754,18 +754,18 @@ def main(argv):
 
                     total_loss += distributed_train_step(ds_one, ds_two)
                     num_batches+=1
-                    if (global_step.numpy()+ 1) % checkpoint_steps==0:
+                    #if (global_step.numpy()+ 1) % checkpoint_steps==0:
                         
-                        with summary_writer.as_default():
-                            cur_step = global_step.numpy()
-                            checkpoint_manager.save(cur_step)
-                            logging.info('Completed: %d / %d steps',
-                                        cur_step, train_steps)
-                            metrics.log_and_write_metrics_to_summary(
-                                all_metrics, cur_step)
-                            tf.summary.scalar('learning_rate', lr_schedule(tf.cast(global_step, dtype=tf.float32)),
-                                            global_step)
-                            summary_writer.flush()
+                    with summary_writer.as_default():
+                        cur_step = global_step.numpy()
+                        checkpoint_manager.save(cur_step)
+                        logging.info('Completed: %d / %d steps',
+                                    cur_step, train_steps)
+                        metrics.log_and_write_metrics_to_summary(
+                            all_metrics, cur_step)
+                        tf.summary.scalar('learning_rate', lr_schedule(tf.cast(global_step, dtype=tf.float32)),
+                                        global_step)
+                        summary_writer.flush()
                 epoch_loss= total_loss/num_batches
                 # Wandb Configure for Visualize the Model Training
                 wandb.log({
