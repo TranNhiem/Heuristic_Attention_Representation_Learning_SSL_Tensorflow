7ffc22fb379d1c8515f718b5ed2d8471b0fade7c
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-11-21 12:12:27 +0800

    commit

--- run_single_machine_baseline.py
+++ run_single_machine_baseline.py
@@ -19,7 +19,7 @@ from imutils import paths
 # Setting GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 if gpus:
 
     try:
