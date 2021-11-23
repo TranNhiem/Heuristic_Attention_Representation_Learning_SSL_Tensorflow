7f41afe4e37581c6c1095adbb77087d04ae9eb44
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-11-23 10:09:38 +0800

    commit

--- run_single_machine_v1.py
+++ run_single_machine_v1.py
@@ -20,6 +20,8 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
 
     try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
         tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
