945c0972a0249277d25629ffb05a954369a398f3
Harry <tigerlittle1@gmail.com>
2021-11-25 03:25:52 +0000

    Noncomtrastive baseline

--- run_single_machine_baseline_non_contrastive.py
+++ run_single_machine_baseline_non_contrastive.py
@@ -20,7 +20,6 @@ from wandb.keras import WandbCallback
 # Setting GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
-
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
