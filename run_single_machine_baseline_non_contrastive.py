ad086032a620740917a394d6ec78b7c281950552
Harry <tigerlittle1@gmail.com>
2021-11-25 02:27:21 +0000

    Noncomtrastive baseline

--- run_single_machine_baseline_non_contrastive.py
+++ run_single_machine_baseline_non_contrastive.py
@@ -24,7 +24,7 @@ if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-        tf.config.experimental.set_visible_devices(gpus[0:4], 'GPU')
+        tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
     except RuntimeError as e:
