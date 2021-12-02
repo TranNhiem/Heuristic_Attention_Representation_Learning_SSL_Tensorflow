6301e0376d9364437b89671ed18a6c0f4b11f6b0
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-02 11:17:48 +0800

    commit

--- config/config.py
+++ config/config.py
@@ -191,18 +191,18 @@ def Projection_and_Prediction_head():
         # Projection & Prediction head  (Consideration the project out dim smaller than Represenation)
 
     flags.DEFINE_integer(
-        'proj_out_dim', 512,
+        'proj_out_dim', 256,
         'Number of head projection dimension.')
 
     flags.DEFINE_integer(
-        'prediction_out_dim', 512,
+        'prediction_out_dim', 256,
         'Number of head projection dimension.')
 
     flags.DEFINE_boolean(
         'reduce_linear_dimention', True,  # Consider use it when Project head layers > 2
         'Reduce the parameter of Projection in middel layers.')
     flags.DEFINE_integer(
-        'up_scale', 4096,  # scaling the Encoder output 2048 --> 4096
+        'up_scale', 2048,  # scaling the Encoder output 2048 --> 4096
         'Upscale the Dense Unit of Non-Contrastive Framework')
 
     flags.DEFINE_boolean(
