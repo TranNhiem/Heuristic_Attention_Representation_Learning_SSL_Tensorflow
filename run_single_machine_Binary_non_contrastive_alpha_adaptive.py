d7a3f8033de21a4b129a541b5868daba9e4b9166
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-14 20:01:12 +0800

    commit

--- run_single_machine_Binary_non_contrastive_alpha_adaptive.py
+++ run_single_machine_Binary_non_contrastive_alpha_adaptive.py
@@ -1,5 +1,5 @@
 
-from config.config_7_7_512_original_binary_loss import read_cfg
+from config.config_14_14_512_original_binary_loss import read_cfg
 #from config.config_for_add_orgloss import read_cfg
 from config.absl_mock import Mock_Flag
 
