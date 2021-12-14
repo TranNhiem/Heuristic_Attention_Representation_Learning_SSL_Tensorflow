5e938b05d5ddc7ee381aecf4ec38bac15035ac48
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-14 09:21:50 +0800

    commit

--- run_single_machine_baseline_non_contrastive.py
+++ run_single_machine_baseline_non_contrastive.py
@@ -29,7 +29,7 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-from config.config_7_7_1024 import read_cfg
+from config.config_7_7_512_baseline import read_cfg
 read_cfg()
 from config.absl_mock import Mock_Flag
 flag = Mock_Flag()
