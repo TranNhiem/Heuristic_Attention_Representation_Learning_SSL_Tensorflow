0d7c0b55f749cc8939833bc62184e98966f82ded
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-20 22:32:25 +0800

    commit

--- config/config_14_14_512_original_binary_loss.py
+++ config/config_14_14_512_original_binary_loss.py
@@ -331,7 +331,7 @@ def Configure_Saving_and_Restore_Model():
     # Saving Model
     flags = Mock_Flag()
     flags.DEFINE_string(
-        'model_dir', "/data1/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_4_alpha_1",
+        'model_dir', "/shared_SSD_20TB/SSL_TEAM/check_point/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_4_alpha_1",
         'Model directory for training.')
 
     flags.DEFINE_integer(
