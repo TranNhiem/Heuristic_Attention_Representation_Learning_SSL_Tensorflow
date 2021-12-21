45cd6d4a1d58a6f350ac0d3613ef8c3809386286
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2021-12-21 13:57:13 +0000

    commit

--- config/config_14_14_512_original_binary_loss.py
+++ config/config_14_14_512_original_binary_loss.py
@@ -54,12 +54,12 @@ def base_cfg():
 
     flags.DEFINE_string(
         #'train_path', "/mnt/sharefolder/Datasets/SSL_dataset/ImageNet/1K_New/ILSVRC2012_img_train",
-        'train_path', '/data1/1K_New/train',
+        'train_path', '/data1/share/1K_New/train',
         'Train dataset path.')
 
     flags.DEFINE_string(
         # 'val_path',"/mnt/sharefolder/Datasets/SSL_dataset/ImageNet/1K_New/val",
-        'val_path', '/data1/1K_New/val',
+        'val_path', '/data1/share/1K_New/val',
         'Validaion dataset path.')
 
     # Mask_folder should locate in location and same level of train folder
@@ -329,8 +329,8 @@ def Configure_Saving_and_Restore_Model():
     # Saving Model
     flags = Mock_Flag()
     flags.DEFINE_string(
-        #'model_dir', "/data1/check_point/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_4_alpha_1",
-        'model_dir', "/shared_SSD_20TB/SSL_TEAM/check_point/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_4_alpha_schedule",
+        'model_dir', "/data1/check_point/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_4_alpha_schedule",
+        #'model_dir', "/shared_SSD_20TB/SSL_TEAM/check_point/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_4_alpha_schedule",
         'Model directory for training.')
 
     flags.DEFINE_integer(
