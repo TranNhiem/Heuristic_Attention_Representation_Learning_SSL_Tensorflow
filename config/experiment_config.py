ff1fba4c23c3f0b9f73e174cbf31dfb6e72b55a7
harry <tigerlittle1@gmail.com>
2022-01-04 15:57:33 +0800

    modify experiment_config.py

--- config/experiment_config.py
+++ config/experiment_config.py
@@ -7,7 +7,7 @@ def read_cfg(mod="non_contrastive"):
     FLAGS = flag.FLAGS
 
     FLAGS.wandb_project_name = "heuristic_attention_represenation_learning_ResNet18"
-    FLAGS.wandb_run_name = "restnet18_output_(7*7*512)_baseline_symloss"
+    FLAGS.wandb_run_name = "MNC_resnet18(14*14*512)_mask_original_loss_Beta_0_5_alpha_schedule_symloss"
     FLAGS.wandb_mod = "run"
 
     FLAGS.Middle_layer_output = None
@@ -17,9 +17,9 @@ def read_cfg(mod="non_contrastive"):
     
     FLAGS.loss_type ="symmetrized"# asymmetrized (2 only options)
     FLAGS.mixprecision='fp32' #['fp16', 'fp32'],  # fp32 is original precision
-    FLAGS.base_lr = 0.3
+    FLAGS.base_lr = 0.5
 
-    FLAGS.non_contrast_binary_loss = "sum_symetrize_l2_loss_object_backg"
+    FLAGS.non_contrast_binary_loss = "sum_symetrize_l2_loss_object_backg_add_original"#sum_symetrize_l2_loss_object_backg_add_original
     FLAGS.alpha = 1
     FLAGS.weighted_loss = 0.5
     FLAGS.resnet_depth = 18
@@ -28,7 +28,7 @@ def read_cfg(mod="non_contrastive"):
 
     FLAGS.train_batch_size = 128
     FLAGS.val_batch_size = 128
-    FLAGS.model_dir = "/data1/share/resnet_byol/restnet18_output_(7*7*512)_baseline_symloss"
+    FLAGS.model_dir = "/data1/share/resnet_byol/MNC_resnet18(14_14_512)_mask_original_loss_Beta_0_5_alpha_schedule_symloss"
     #FLAGS.train_mode = "finetune"
 
 
