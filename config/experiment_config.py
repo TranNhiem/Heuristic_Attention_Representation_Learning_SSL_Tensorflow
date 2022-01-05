9b044482bf951208995e60597aa8cf86c62f28d1
harry <tigerlittle1@gmail.com>
2022-01-05 08:58:57 +0800

    modify experiment_config.py

--- config/experiment_config.py
+++ config/experiment_config.py
@@ -7,19 +7,19 @@ def read_cfg(mod="non_contrastive"):
     FLAGS = flag.FLAGS
 
     FLAGS.wandb_project_name = "heuristic_attention_representation_learning_Paper"
-    FLAGS.wandb_run_name = "Baseline_(7_7_2048)_100epoch_symloss"
+    FLAGS.wandb_run_name = "MNC_(14_14_2048)_100epoch_alpha_schedule_symloss"
     FLAGS.wandb_mod = "run"
 
     FLAGS.Middle_layer_output = None
     FLAGS.original_loss_stop_gradient = False
-    FLAGS.Encoder_block_strides = {'1':2,'2':1,'3':2,'4':2,'5':2}
+    FLAGS.Encoder_block_strides = {'1':2,'2':1,'3':2,'4':2,'5':1}
     FLAGS.Encoder_block_channel_output = {'1':1,'2':1,'3':1,'4':1,'5':1}
     
     FLAGS.loss_type ="symmetrized"# asymmetrized (2 only options)
     FLAGS.mixprecision='fp32' #['fp16', 'fp32'],  # fp32 is original precision
-    FLAGS.base_lr = 0.3
+    FLAGS.base_lr = 0.5
 
-    FLAGS.non_contrast_binary_loss = "Baseline loss"#sum_symetrize_l2_loss_object_backg_add_original sum_symetrize_l2_loss_object_backg_add_original
+    FLAGS.non_contrast_binary_loss = "sum_symetrize_l2_loss_object_backg"#sum_symetrize_l2_loss_object_backg_add_original
     FLAGS.alpha = 1
     FLAGS.weighted_loss = 0.5
     FLAGS.resnet_depth = 50
@@ -28,7 +28,7 @@ def read_cfg(mod="non_contrastive"):
 
     FLAGS.train_batch_size = 128
     FLAGS.val_batch_size = 128
-    FLAGS.model_dir = "/data1/share/resnet_byol/resnet50/Baseline_(7_7_2048)_100epoch_symloss"
+    FLAGS.model_dir = "/data1/share/resnet_byol/resnet50/MNC_(14_14_2048)_100epoch_alpha_schedule_symloss"
     #FLAGS.train_mode = "finetune"
 
 
