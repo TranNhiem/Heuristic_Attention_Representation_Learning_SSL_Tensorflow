5410d0a532939c929fdafffc2a4e45f72c33879a
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-01-15 15:14:30 +0800

    fp16_test

--- config/experiment_config.py
+++ config/experiment_config.py
@@ -27,7 +27,8 @@ def read_cfg(mod="non_contrastive"):
     FLAGS.moving_average = "schedule"
     # ['fp16', 'fp32'],  # fp32 is original precision
     FLAGS.mixprecision = 'fp16'
-    FLAGS.precision_method= 'API' # ['API', custome]
+    FLAGS.precision_method = 'API'  # ['API', custome]
+    FLAGS.aggregate_loss = "contrastive"
     FLAGS.base_lr = 0.5
 
     # sum_symetrize_l2_loss_object_backg_add_original
