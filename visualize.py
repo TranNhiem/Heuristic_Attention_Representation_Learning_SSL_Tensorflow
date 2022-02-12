6e5806968560c477a445ed371f1ae28a2237cbde
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-02-12 22:47:58 +0800

    helper_function

--- visualize.py
+++ visualize.py
@@ -11,14 +11,15 @@ import os
 import math
 import errno
 import shutil
-from helper_functions import *
+#from helper_functions import *
+
 
 class Visualize:
-    def __init__(self,epoch,visualize_dir):
+    def __init__(self, epoch, visualize_dir):
         self.epoch = epoch
         self.visualize_dir = visualize_dir
 
-    def plot_feature_map(self,epoch,features,mask = None):
+    def plot_feature_map(self, epoch, features, mask=None):
         square = 5
         ix = 1
         if mask != None:
@@ -28,7 +29,7 @@ class Visualize:
         for i in range(square):
             for j in range(square):
                 ax = plt.subplot(square, square, ix)
-                f = features[0,:,:,ix-1]
+                f = features[0, :, :, ix-1]
                 f_max = np.max(f)
                 f_min = np.min(f)
                 f = (((f-f_min)/(f_max-f_min))*255.0).astype(np.uint8)
@@ -37,13 +38,8 @@ class Visualize:
                 # plot filter channel in grayscale
                 if mask != None:
                     mask = tf.image.resize(mask, (f.shape[0], f.shape[1]))
-                    f = tf.multiply(f, mask[:,:,0])
+                    f = tf.multiply(f, mask[:, :, 0])
                 plt.imshow(f, cmap='gray')
                 ix += 1
-        plt.savefig(os.path.join(self.visualize_dir,str(epoch)+".png"))
-        print("save in : ",os.path.join(self.visualize_dir,str(epoch)+".png"))
-
-
-
-
-
+        plt.savefig(os.path.join(self.visualize_dir, str(epoch)+".png"))
+        print("save in : ", os.path.join(self.visualize_dir, str(epoch)+".png"))
