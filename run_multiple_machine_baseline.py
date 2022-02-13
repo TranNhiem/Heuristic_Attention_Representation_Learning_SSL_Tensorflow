838645e17ef891873912b422d47696f0169632c0
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-02-13 18:19:46 +0800

    configure

--- run_multiple_machine_baseline.py
+++ run_multiple_machine_baseline.py
@@ -32,6 +32,16 @@ read_cfg()
 flag = Mock_Flag()
 FLAGS = flag.FLAGS
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        print(e)
 
 if not os.path.isdir(FLAGS.model_dir):
     print("creat : ", FLAGS.model_dir, FLAGS.cached_file_val, FLAGS.cached_file)
