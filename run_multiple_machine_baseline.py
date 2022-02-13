823bf821aa21af46ac75db7add4c1a132f4dacc5
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-02-13 20:55:03 +0800

    gpus

--- run_multiple_machine_baseline.py
+++ run_multiple_machine_baseline.py
@@ -27,6 +27,14 @@ from multiprocessing import util
 from config.absl_mock import Mock_Flag
 from config.experiment_config_multi_machine import read_cfg
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
 
 read_cfg()
 flag = Mock_Flag()
