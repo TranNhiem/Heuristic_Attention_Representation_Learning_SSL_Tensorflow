6147ffab09f80be8ade8bf245d315cb79206ae50
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-02-14 17:31:57 +0800

    config

--- run_multi_MNCRL.py
+++ run_multi_MNCRL.py
@@ -30,6 +30,8 @@ if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 #         tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
 #         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
 #         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
