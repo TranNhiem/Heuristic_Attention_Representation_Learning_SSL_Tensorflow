62ea29da521bb20b368d86c22769706158a3047b
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-02-16 15:26:35 +0800

    config

--- run_multiple_machine_baseline.py
+++ run_multiple_machine_baseline.py
@@ -78,8 +78,8 @@ def main():
     else:
         raise ValueError("Invalida communication method")
 
-    strategy = tf.distribute.MultiWorkerMirroredStrategy(
-    )  # communication_options=communication_options  #
+    strategy = tf.distribute.MultiWorkerMirroredStrategy(ommunication_options=communication_options, cluster_resolver=None
+                                                         )  # communication_options=communication_options  #
     # strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
     # ------------------------------------------
     # Preparing dataset
