081a35a79157427371d341131ca6b86b957a89dc
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-02-18 15:48:54 +0800

    config

--- config/experiment_config_multi_machine.py
+++ config/experiment_config_multi_machine.py
@@ -47,6 +47,6 @@ def read_cfg(mod="non_contrastive"):
     FLAGS.subset_percentage = 1.0
     FLAGS.per_gpu_train_batch = 256
     FLAGS.per_gpu_val_batch = 256
-    FLAGS.model_dir = "./resnet_byol/resnet18_correction/test_multi_machine_11"
+    FLAGS.model_dir = "./resnet_byol/resnet18_correction/test_multi_machine_22"
 
     #FLAGS.train_mode = "finetune"
